c8ea6cbcf304a4ca9bad05d841a64ac424a6199b
Nikita Gezha <ngez.work@gmail.com>
2023-09-01 19:59:45 +0300

    typo fix

--- model_running/eval_results_callback.py
+++ model_running/eval_results_callback.py
@@ -64,7 +64,7 @@ class EvaluationResultsCallback:
     model_answer_id = self._get_reading_comprehension_answer_id_from_model_output(raw_output)
     correct_answer_letter = self.test_data[input_code]['answer']
     correct_answer_id = self.alphabet2idx[correct_answer_letter]
-    correct = model_answer_id is not None and model_answer_id == correct_answer_id:
+    correct = model_answer_id is not None and model_answer_id == correct_answer_id
     processed_output = {
       'interpreted_output': model_answer_id,
       'model_output': raw_output,
