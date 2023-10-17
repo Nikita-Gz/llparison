eb6ad7776fba4fd128341b89fdf348c4458d52c8
Nikita-Gz <ngez.work@gmail.com>
2023-10-17 23:51:28 +0300

    BASIC_MATH changed to BASIC_MULTIPLICATION

--- model_running/task_type.py
+++ model_running/task_type.py
@@ -2,20 +2,20 @@ from enum import Enum
 
 class TaskType(Enum):
   READING_COMPREHENSION = 1
-  BASIC_MATH = 2
+  BASIC_MULTIPLICATION = 2
   BOT_DETECTION = 4
 
 task_type_int_to_str = {
   TaskType.READING_COMPREHENSION: 'Reading Comprehension',
   TaskType.BOT_DETECTION: 'Bot Detection',
-  TaskType.BASIC_MATH: 'Basic Math'
+  TaskType.BASIC_MULTIPLICATION: 'Basic Math'
 }
 task_type_str_to_int = {s:i for i, s in task_type_int_to_str.items()}
 
 new_tokens_limit_per_task_type_int = {
   TaskType.READING_COMPREHENSION: 3,
   TaskType.BOT_DETECTION: 1,
-  TaskType.BASIC_MATH: 5
+  TaskType.BASIC_MULTIPLICATION: 5
 }
 
 new_tokens_limit_per_task_type_str = {s:new_tokens_limit_per_task_type_int[i] for i, s in task_type_int_to_str.items()}
